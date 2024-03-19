dc2f8bcbdfebec2168b0011a1cc2a8ec4d6510b6
Nguyễn Cao An <nca1407@gmail.com>
2024-03-19 22:26:17 +0700

    fix loi khong lay duoc du lieu tu db

--- index.php
+++ index.php
@@ -1,22 +1,40 @@
 <!DOCTYPE html>
+
 <html>
+
 <head>
-    <title>BÁO CÁO THỰC TẬP CHUYÊN MÔN: ỨNG DỤNG PHP TẠO TRANG ĐĂNG NHẬP ĐƠN GIẢN</title>
+
+    <title>LOGIN</title>
 
     <link rel="stylesheet" type="text/css" href="style.css">
+
+    <meta charset="UTF-8">
 </head>
+
 <body>
+
      <form action="login.php" method="post">
+
         <h2>LOGIN</h2>
-        //Kiểm tra trong URL có lỗi hay không
+
         <?php if (isset($_GET['error'])) { ?>
+
             <p class="error"><?php echo $_GET['error']; ?></p>
+
         <?php } ?>
-        <label>Tên Đăng Nhập</label>
-        <input type="text" name="username" placeholder="Nhập Tên Đăng Nhập"><br>
-        <label>Mật Khẩu</label>
-        <input type="password" name="password" placeholder="Nhập Mật Khẩu"><br> 
-        <button type="submit">Đăng Nhập</button>
+
+        <label>User Name</label>
+
+        <input type="text" name="uname" placeholder="Nhập tên đăng nhập"><br>
+
+        <label>Password</label>
+
+        <input type="password" name="password" placeholder="Nhập mật khẩu"><br> 
+
+        <button type="Đăng Nhập">Login</button>
+
      </form>
+
 </body>
+
 </html>
